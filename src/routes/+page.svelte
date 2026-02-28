ef67e52b14af88c855c8c986d367a7af001aaf69
avril-matute <avmatute@mit.edu>
2026-02-28 16:46:16 -0500

    starting lab 3!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<title>"Avril Matute: Personal Portfolio"</title>
+    <h1> Avril Matute </h1>
+    <img src="images/IMG_5547.JPEG" alt="Me in a museum smiling" width="70%">
+    <p> Hi, I am an 11-6 from Honduras :)</p>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,5 @@
 <svelte:head>
   <title>Contact me</title>
 </svelte:head>
+
+<h1> Contact me </h1>
